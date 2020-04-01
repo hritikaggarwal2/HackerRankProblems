1f9d4e0b0b665dc6add911316a321da59ab35ee8
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2020-04-01 15:56:59 -0400

    add java basics

---
+++ Java/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextInt());
+        System.out.println(scan.nextInt());
+        System.out.println(scan.nextInt());
+    }
+}
