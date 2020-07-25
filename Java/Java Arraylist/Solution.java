c682b31b021daae650f76324662f44ecbc13d274
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2020-07-25 01:47:11 -0700

    added to data structures

---
+++ Java/Java Arraylist/Solution.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        List<ArrayList<Integer>> outerList = new ArrayList<ArrayList<Integer>>();
+        int rowCountA = scanner.nextInt();
+
+        for (int i = 0; i < rowCountA; i++) {
+            ArrayList<Integer> innerList = new ArrayList<Integer>();
+            int colCount = scanner.nextInt();
+
+            for (int j = 0; j < colCount; j++) {
+                innerList.add(scanner.nextInt());
+            }
+
+            outerList.add(innerList);
+        }
+
+        int rowCountB = scanner.nextInt();
+
+        for (int i = 0; i < rowCountB; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            try {
+                int num = outerList.get(a - 1).get(b - 1);
+                System.out.println(num);
+            } catch (Exception E) {
+                System.out.println("ERROR!");
+            }
+
+        }
+
+    }
+}
