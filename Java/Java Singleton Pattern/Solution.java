3e76c6b74ab4b33a175a607042952174c6f56ae5
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2020-09-24 12:50:23 -0700

    added 6 new to java advanced

---
+++ Java/Java Singleton Pattern/Solution.java
@@ -0,0 +1,15 @@
+class Singleton {
+    private static Singleton instance;
+    public String str;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getSingleInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
