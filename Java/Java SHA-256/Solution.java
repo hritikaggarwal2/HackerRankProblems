55db6f354decd80739a43475d46ef651d0ae2c36
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2020-09-25 23:37:19 -0700

    added 1 new to java advanced

---
+++ Java/Java SHA-256/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.security.MessageDigest;
+
+public class Solution {
+    public static void main(String[] args) throws Throwable {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner scan = new Scanner(System.in);
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        String toEncode = scan.next();
+        scan.close();
+
+        byte[] mdBytes = md.digest(toEncode.getBytes());
+
+        for (byte perByte : mdBytes) {
+            System.out.printf("%02x", perByte);
+        }
+
+        System.out.println();
+    }
+}
